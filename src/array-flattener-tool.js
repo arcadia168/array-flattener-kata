19ecc07fd5835b74ac80b2b9dcd958cbaf2035bd
Erin Eland <erin@erineland.com>
2019-06-03 22:30:44 +0100

    Initial commits to the repo for progress saving.

---
+++ src/array-flattener-tool.js
@@ -0,0 +1,3 @@
+module.exports = arrayToFlatten => {
+    return arrayToFlatten;
+}
---
+++ tests/array_flatten.spec.js
@@ -0,0 +1,9 @@
+const ArrayFlattenerTool = require('../src/array-flattener-tool.js');
+
+describe('Array flattening tool', () => {
+    describe('When passed an array', () => {
+        it('Returns an array', () => {
+
+        });
+    });
+});
