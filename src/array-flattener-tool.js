01c438ed990941fd979fc17028c737258f3fdcb4
Erin Eland <erin@erineland.com>
2019-06-04 10:29:05 +0100

    Removing console.log statements.

--- src/array-flattener-tool.js
+++ src/array-flattener-tool.js
@@ -1,11 +1,7 @@
 const flattenArray = (arrayToFlatten, newFlatArray) => {
-    console.log(`Inside recursive function! arrayToFlatten is: ${arrayToFlatten}`);
     if (arrayToFlatten.toString().length == 1) {
-        console.log(`BASE CASE!: ${arrayToFlatten}`);
         newFlatArray.push(arrayToFlatten);
-        console.log(`The newFlatArray is now: ${newFlatArray}`);
     } else {
-        console.log(`RECURSIVE CASE!`)
         arrayToFlatten.forEach(arrayElement => {
             flattenArray(arrayElement, newFlatArray);
         });
@@ -13,17 +9,12 @@ const flattenArray = (arrayToFlatten, newFlatArray) => {
 }
 
 module.exports = arrayToFlatten => {
-
     if (!arrayToFlatten) {
         throw new Error('Please ensure you pass a valid array to the tool.');
     }
 
-    console.info(`arrayToFlatten is: ${arrayToFlatten}`);
-
     const flatArray = [];
     flattenArray(arrayToFlatten, flatArray);
-
-    console.info(`finalFlatArray is: ${flatArray}`);
 
     return flatArray;
 }
